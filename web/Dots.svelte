357082282d2af3f3d4598617a4a563a7cccff5bb
Kyle Rudy <kylerudy@gmail.com>
2022-07-16 03:36:44 -0700

    can't sleep

---
+++ web/Dots.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let value = 1;
+    export let maximum = 5;
+	const filled_character = '⬤';
+	const empty_character = '○';
+    function setValue(num) {
+		value = num;
+	}
+</script>
+
+{#each Array(maximum) as _, index (index) }
+	<span on:click={setValue(index + 1)}>
+    {#if index < value}
+		{filled_character}
+	{:else}
+		{empty_character}
+    {/if}
+	</span>
+{/each}
+
+<style>
+	span 
+    {
+		cursor:pointer;
+		font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+	}
+</style>
